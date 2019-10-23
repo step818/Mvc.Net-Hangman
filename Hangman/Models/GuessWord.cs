4178a0d30ff0e175bf96fe47a580cb7888df2d5e
Stephen Trewick <stephent818@gmail.com>
2019-10-23 01:42:35 -0700

    created instance in the model

--- Hangman/Models/GuessWord.cs
+++ Hangman/Models/GuessWord.cs
@@ -6,6 +6,25 @@ namespace Hangman.Models
   public class MysteryWord
   {
     private static List<MysteryWord> _words = new List<MysteryWord>{};
+    private static List<MysteryWord> _letters = new List<MysteryWord>{};
+    public int Id { get;set; }
     public string Word { get; set; }
+    public MysteryWord(string word)
+    {
+      Word = word;
+      _words.Add(this);
+      Id = _words.Count;
+    }
+    public bool CheckLetter(char letter)
+    {
+      if(Word.Contains(letter))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
   }
 }
